407a9b39dc0918954d7bce3d2379aa9950a7b24c
Vinicius Maeda <viniciusmaeda@gmail.com>
2024-04-17 11:26:25 -0400

    Adicionado suavização anisotrópica

--- atividadeRuidosArgs.py
+++ atividadeRuidosArgs.py
@@ -1,5 +1,6 @@
 # Programa desenvolvido para a disciplina de Visão Computacional.
 # Professor: Dr. Hemerson Pistori
+# Atividade: Ruídos - Implementar um programa que aplique ruídos, suavize e detecte bordas em uma imagem.
 #
 # Autor: Vinícius Maeda (viniciusmaeda@gmail.com)
 #
@@ -10,7 +11,7 @@
 from argparse import ArgumentParser
 from skimage import io
 from skimage import filters, feature
-from skimage.color import rgb2gray
+from skimage.restoration import denoise_tv_bregman
 from skimage.util import img_as_ubyte, random_noise
 import matplotlib.pyplot as plt
 import cv2 as cv
@@ -21,29 +22,36 @@ import cv2 as cv
 # função para aplicar ruído na imagem
 def aplicarRuido(img, ruido, parametro):
   if (ruido == 'gauss'):
-   # aplica ruído gaussiano na imagem
-   imgRuido = random_noise(img, mode = 'gaussian')
+    # caso não seja fornecido o parâmetro, será definido como 0.01 (padrão)
+    parametro = 0.01 if parametro == None else parametro
+    # aplica ruído gaussiano na imagem
+    imgRuido = random_noise(img, mode = 'gaussian', var = parametro/100)
   elif (ruido == 'sep'):
-   # aplica ruído sal e pimenta na imagem
-   imgRuido = random_noise(img, mode = 's&p', amount = parametro/100)
+    # caso não seja fornecido o parâmetro, será definido como 0.05 (padrão)
+    parametro = 0.05 if parametro == None else parametro
+    # aplica ruído sal e pimenta na imagem
+    imgRuido = random_noise(img, mode = 's&p', amount = parametro/100)
   elif (ruido == 'poisson'):
-   # aplica ruído de poisson na imagem
-   imgRuido = random_noise(img, mode = 'poisson')
+    # aplica ruído de poisson na imagem
+    imgRuido = random_noise(img, mode = 'poisson')
 
   return imgRuido
 
 
 
-def aplicarSuavizacao(img, filtro, ksize):
+def aplicarSuavizacao(img, filtro, parametro):
   if (filtro == 'mean'):
     # aplica um filtro de média para suavizar a imagem
-    imgSuavizada = cv.blur(img, (ksize, ksize))
+    imgSuavizada = cv.blur(img, (parametro, parametro))
   elif (filtro == 'median'):
     # aplica um filtro de mediana para suavizar a imagem
-    imgSuavizada = cv.medianBlur(img_as_ubyte(img), ksize)
+    imgSuavizada = cv.medianBlur(img_as_ubyte(img), parametro)
   elif (filtro == 'gauss'):
     # aplica um filtro Gaussiano para suavizar a imagem
-    imgSuavizada = cv.GaussianBlur(imagemComRuido, (ksize, ksize), cv.BORDER_DEFAULT)
+    imgSuavizada = cv.GaussianBlur(imagemComRuido, (parametro, parametro), cv.BORDER_DEFAULT)
+  elif (filtro == 'anisotropico'):
+    # aplicar o filtro anisotrópico
+    imgSuavizada = denoise_tv_bregman(img, weight = parametro/100)
 
   return imgSuavizada   
 
@@ -53,9 +61,9 @@ def extrairBordas(img, borda, parametro):
   if (borda == 'sobel'):
     imgBordas = filters.sobel(img)
   elif (borda == 'scharr'):
-    imgBordas = feature.canny(img, sigma = parametro)
-  elif (borda == 'canny'):
     imgBordas = filters.scharr(img)
+  elif (borda == 'canny'):
+    imgBordas = feature.canny(img, sigma = parametro)
 
   return imgBordas
 
@@ -69,7 +77,7 @@ def leituraDosArgumentos():
                       help="Imagem a ser processada")
   parser.add_argument("-r", "--ruido", default="gauss",
                       help="Tipo de ruído. Pode ser gauss, sep ou poisson")
-  parser.add_argument("-pr", "--pruido", default=9,type=int,
+  parser.add_argument("-pr", "--pruido", type=int,
                       help="Parâmetro do ruído. Depende do tipo do ruído")
   parser.add_argument("-s", "--suavizador", default="gauss",
                       help="Tipo de suavizador. Pode ser mean, median ou gauss")
@@ -109,8 +117,8 @@ if (__name__ == "__main__"):
   # Abre a imagem de entrada
   imagemOriginal = io.imread(pastaImagem + argumentos.imagem)
 
-  # Converte para tons de cinza
-  imagemCinza = rgb2gray(imagemOriginal)
+  # # Converte para tons de cinza
+  imagemCinza = cv.cvtColor(imagemOriginal, cv.COLOR_BGR2GRAY)
 
   # Aplica o ruído na imagem dependendo do tipo de ruído escolhido
   imagemComRuido = aplicarRuido(imagemCinza, argumentos.ruido, argumentos.pruido)
@@ -121,10 +129,23 @@ if (__name__ == "__main__"):
   # Aplica detecção de borda
   imagemBorda = extrairBordas(imagemSuavizada, argumentos.borda, argumentos.pborda)
 
+  # Dicionário para traduzir os termos
+  dicionarioTermos = {
+    'gauss': 'Gaussiano',
+    'sep': 'Sal e Pimenta',
+    'poisson': 'Poisson',
+    'mean': 'Média',
+    'median': 'Mediana',
+    'anisotropico': 'Anisotrópica',
+    'sobel': 'Sobel',
+    'scharr': 'Scharr',
+    'canny': 'Canny'
+  }
+
   # Mostra as imagens com os resultados numa janela
   if argumentos.interface == "sim":
 
-    fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 5),sharex=True, sharey=True)
+    fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(14, 5), sharex=True, sharey=True)
 
     ax = axes.ravel()
 
@@ -134,15 +155,19 @@ if (__name__ == "__main__"):
 
     ax[1].imshow(imagemComRuido,cmap='gray')
     ax[1].axis('off')
-    ax[1].set_title('Com Ruído = ' + str(argumentos.ruido) + ' ' + str(argumentos.pruido))
+    tituloRuido = 'Ruído = ' + dicionarioTermos[argumentos.ruido]
+    tituloRuido += '\nParâmetro: ' + (str(argumentos.pruido) if (argumentos.ruido == 'sep' or argumentos.ruido == 'gauss') else '-')
+    ax[1].set_title(tituloRuido)
 
     ax[2].imshow(imagemSuavizada,cmap='gray')
     ax[2].axis('off')
-    ax[2].set_title('Suavizada = ' + str(argumentos.suavizador) + ' ' + str(argumentos.psuavizador))
+    ax[2].set_title('Suavização = ' + str(dicionarioTermos[argumentos.suavizador]) + '\nParâmetro: ' + str(argumentos.psuavizador))
 
     ax[3].imshow(imagemBorda,cmap='gray')
     ax[3].axis('off')
-    ax[3].set_title('Borda = ' + str(argumentos.borda) + ' ' + str(argumentos.pborda))
+    tituloBorda = 'Borda = ' + str(dicionarioTermos[argumentos.borda])
+    tituloBorda += '\nParâmetro: ' + (str(argumentos.pborda) if (argumentos.borda == 'canny') else '-')
+    ax[3].set_title(tituloBorda)
 
     fig.tight_layout()
 
@@ -151,5 +176,5 @@ if (__name__ == "__main__"):
 
   # Salva no disco as imagens resultantes (falta implementar)
   io.imsave(pastaImagem + argumentos.imagem.split('.')[0]+"_ruido.jpg", img_as_ubyte(imagemComRuido))
-  io.imsave(pastaImagem + argumentos.imagem.split('.')[0]+"_suavizada.jpg", imagemSuavizada)
+  io.imsave(pastaImagem + argumentos.imagem.split('.')[0]+"_suavizada.jpg", img_as_ubyte(imagemSuavizada))
   io.imsave(pastaImagem + argumentos.imagem.split('.')[0]+"_borda.jpg", img_as_ubyte(imagemBorda))
